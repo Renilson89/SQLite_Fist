4ce5dbc8168629a1ec0bad91669392bcf4a1a667
Renilson Marques <nilsonmarques2015@hotmail.com>
2024-04-02 11:51:23 -0400

    continuação do SQLite

--- app/src/main/java/com/example/bd_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/bd_sqlite/MainActivity.kt
@@ -2,9 +2,16 @@ package com.example.bd_sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.example.bd_sqlite.database.DatabaseHelper
+import com.example.bd_sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     private val bancoDados by lazy {
         DatabaseHelper(this)
@@ -12,14 +19,50 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        try {
-            bancoDados.writableDatabase.execSQL(
-                "INSERT INTO produtos VALUES(null, 'Note', 'Descricação.');"
-            )
-        }catch (e: Exception){
+        with(binding){
+            btnSalvar.setOnClickListener{
+                salvar()
+            }
+            btnListar.setOnClickListener {
+                Listar()
+            }
+  }
 
+    }
+
+    private fun Listar(){
+
+        val sql = "SELECT * FROM ${DatabaseHelper.TABELA_PRODUTOS};"
+        val cursor = bancoDados.readableDatabase.rawQuery(sql, null)
+
+        val indiceId = cursor.getColumnIndex("${DatabaseHelper.ID_PRODUTOS}")
+        val indiceTitulo = cursor.getColumnIndex("${DatabaseHelper.TITULO}")
+        val indiceDescricao = cursor.getColumnIndex("${DatabaseHelper.DESCRICAO}")
+
+        while (cursor.moveToNext()){
+           val idProduto = cursor.getInt(indiceId)
+            val titulo = cursor.getString(indiceTitulo)
+            val descricao = cursor.getString(indiceDescricao)
+            Log.i("info_db", "id: $idProduto - $titulo")
         }
     }
-}
+
+    private fun salvar() {
+
+        val titulo = binding.editProduto.textAlignment.toString()
+        val sql = "INSERT INTO produtos VALUES(null, '$titulo', 'Descricação.');"
+        try {
+            bancoDados.writableDatabase.execSQL(sql)
+            Log.i("info_db", "Sucesso ao Inserir")
+
+        }catch (e: Exception){
+            Log.i("info_db", "Erro ao Inserir")
+        }
+    }
+
+
+    }
+
+
--- app/src/main/java/com/example/bd_sqlite/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/bd_sqlite/database/DatabaseHelper.kt
@@ -8,11 +8,18 @@ import android.util.Log
 
 class DatabaseHelper(context: Context): SQLiteOpenHelper(context, "loja.db",null,1) {
 
+    companion object{
+        const val TABELA_PRODUTOS = "produtos"
+        const val ID_PRODUTOS = "id_produto"
+        const val TITULO = "titulo"
+        const val DESCRICAO = "descricao"
+    }
+
     override fun onCreate(db: SQLiteDatabase?) {
-        val sql = "CREATE TABLE IF NOT EXISTS produtos(\n" +
-                "\tid_produto integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-                "  \ttitulo varchar(100),\n" +
-                "  \tdescricao text\n" +
+        val sql = "CREATE TABLE IF NOT EXISTS $TABELA_PRODUTOS(\n" +
+                "\t$ID_PRODUTOS integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+                "  \t$TITULO varchar(100),\n" +
+                "  \t$DESCRICAO text\n" +
                 "  \n" +
                 ");"
 
